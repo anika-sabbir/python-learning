6de7c915cba919f5342014b31d4d89222d4ef0e9
Anika Sabbir <anikasabbir26@gmail.com>
2026-06-03 17:51:45 +0600

    python basic.py

--- python basic.py/list & tuples.py
+++ python basic.py/list & tuples.py
@@ -97,3 +97,85 @@ else:
 
 
 
+#Problem 1 — Convert(Create a list of 5 numbers. Convert it into a tuple and print both)
+number = []
+number.append(int(input("enter 1st number")))
+number.append(int(input("enter 2nd number")))
+number.append(int(input("enter 3rd number")))
+number.append(int(input("enter 4th number")))
+number.append(int(input("enter 5th number")))
+number_tuple=tuple(number)
+print("list:",number)
+print("tuple:",number_tuple)
+
+
+#Problem 2 — Access(Create a tuple of 3 cities. Add them one by one into an empty list using .append().)
+cities=("Chattogram","Dhaka","Sylhet")
+city_list =[]
+for city in cities:
+    city_list.append(city)
+    print("city list:",city_list)
+print("end of code")
+
+#Problem 3 — Length Compare(Create a list and a tuple each with 4 items. Print the length of both and check if they are equal.)
+
+my_list = [1, 2, 3, 4]
+my_tuple = (5, 6, 7, 8)
+print("List length:", len(my_list))
+print("Tuple length:", len(my_tuple))
+print("Equal?", len(my_list) == len(my_tuple))
+
+#Problem 4 — Search(Create a fruit list and vegetable tuple. Check if user input exists in either.)
+fruits = ["mango", "banana", "apple"]
+vegetables = ("carrot", "potato", "onion")
+food = input("Enter a food item: ").lower()
+if food in fruits:
+    print(food, "is in the fruits list!")
+elif food in vegetables:
+    print(food, "is in the vegetables tuple!")
+else:
+    print(food, "not found in either.")
+
+
+
+#Problem5-Combine(Create a tuple and list of 3 numbers each. Convert tuple to list and combine both.)
+t = (10, 20, 30)
+l = [40, 50, 60]
+combined = list(t) + l
+print("Combined List:", combined)
+
+#Create a tuple of 4 numbers. Print the first and second elements separately.
+tup = (2,1,5,7)
+print(tup[0])
+print(tup[1])
+
+#Create a tuple with only one element. Print it and its type.
+tup=(1,)
+print(tup)
+print(type(tup))
+
+#Create a tuple of 4 numbers. Print the tuple and its type
+tup=(1,2,3,4)
+print(tup)
+print(type(tup))
+
+#Create a tuple of 4 numbers. Print elements from index 1 to 2 using slicing.
+tup=(1,2,3,4)
+print(tup[1:3])
+
+#Create a tuple of 4 numbers. Find and print the index of value 2.
+tup=(1,2,3,4)
+print(tup.index(2))
+
+#Create a tuple where 2 appears twice. Count and print how many times 2 appears
+tup=(1,2,3,2,4)
+print(tup.count(2))
+
+#Write a programme to count the number of student with the "A" grade in the following tuple.
+grade=("C","B","A","D","A","B","A")
+print(grade.count("A"))
+
+#Store the above values in a list & sort them from "A" to "D"
+grade=["C","D","A","A","B","B","A"]
+grade.sort()
+print(grade)
